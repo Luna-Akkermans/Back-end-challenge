9bfab350dd8660ced44e8a0a81ff38b2ad16b2c7
Luna Mila <9904466@mydavinci.nl>
2022-02-21 09:34:59 +0100

    added first-card

--- index.php
+++ index.php
@@ -10,16 +10,17 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <main class="container-fluid w-100 h-100">
-        <div class="container">
+    <main class="container-fluid w-100 h-100 my-4 ">
+        <div class="container d-flex justify-content-center">
             <div class="row">
                 <div class="col-lg-4">
-                    <div class="card">
-                        <div class="card-body d-flex justify-content-center">
-                            <span><i class="fa-solid fa-plus"></i></span>
+                    <div class="card min-Hcard">
+                        <div class="card-body d-flex justify-content-center align-items-center">
+                            <span><i class="display-4 fa-solid fa-plus"></i></span>
                         </div>
                     </div>
                 </div>
