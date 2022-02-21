85f9e25328c0ca2811eea49081ee5211605dbbea
Luna Mila <9904466@mydavinci.nl>
2022-02-21 18:09:31 +0100

    added files

--- sources/database/db_conn.php
+++ sources/database/db_conn.php
@@ -2,7 +2,7 @@
 
 function dbConn(){
     try {
-        $pdo = new PDO("mysql:host=localhost;dbname=planningstool", "root", "mysql");
+        $pdo = new PDO("mysql:host=localhost;dbname=todolist", "root", "mysql");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
         } catch(PDOException $e) {
---
+++ sources/functions/create.php
@@ -0,0 +1,7 @@
+<?php 
+
+
+
+function CreateTable() {
+    $pdo = dbConn();
+}
