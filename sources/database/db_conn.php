1f6a6d613202b41f9edbe33c1ef9af705894e27b
Luna Mila <9904466@mydavinci.nl>
2022-02-21 17:54:54 +0100

    added

---
+++ sources/database/db_conn.php
@@ -0,0 +1,11 @@
+<?php 
+
+function dbConn(){
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=planningstool", "root", "mysql");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+        } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+        }
+    }
